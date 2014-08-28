17a97f633ac07516e195342b14c3dd06a0886bce
Gabriel Wong <me@gabrielwong.net>
2014-08-28 10:56:08 -0700

    Add audio file type

--- test/file-types_spec.js
+++ test/file-types_spec.js
@@ -3,5 +3,9 @@ var expect = chai.expect;
 describe('fileType', function() {
   it('returns the type of the file', function() {
     expect(fileType('application/msword')).to.eq('doc');
+    expect(fileType('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')).to.eq('xls');
+    expect(fileType('application/zip')).to.eq('zip');
+    expect(fileType('audio/MP4A-LATM')).to.eq('audio');
+    expect(fileType('audio/mp4')).to.eq('audio');
   });
 });
